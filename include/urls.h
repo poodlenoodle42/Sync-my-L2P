89c5c44d094edc8344ff04b106340fc2a45b7fce
JayBraker <jaybraker1@gmail.com>
2022-01-07 17:37:31 +0100

    Revert massive commit to be split into more detailed changes

--- include/urls.h
+++ include/urls.h
@@ -3,16 +3,6 @@
 
 #include <QString>
 
-/// URLS sind deprecated seit Abschaltung der L2P Ende 2021
-/* const QString l2pApiDomain = "https://www3.elearning.rwth-aachen.de/";
-const QString l2pApiUrl = l2pApiDomain + "_vti_bin/L2PServices/api.svc/v1/";
-const QString l2pApiDocs = l2pApiUrl + "Documentation";
-const QString viewAllCourseInfoByCurrentSemesterUrl = l2pApiUrl + "viewAllCourseInfoByCurrentSemester";
-const QString viewAllCourseInfoUrl = l2pApiUrl + "viewAllCourseInfo";
-const QString viewActiveFeaturesUrl = l2pApiUrl + "viewActiveFeatures";
-const QString l2pDownloadFileUrl = l2pApiUrl + "downloadFile/";
-const QString moodleMainDomain = "https://www1.elearning.rwth-aachen.de/";
-const QString moodleMainUrl = moodleMainDomain + "pluginfile.php"; */
 const QString moodleApiDomain = "https://moped.ecampus.rwth-aachen.de/";
 const QString moodleApiUrlBase = moodleApiDomain + "proxy/api/v2/";
 const QString moodleApiDocs = moodleApiUrlBase + "Documentation";
